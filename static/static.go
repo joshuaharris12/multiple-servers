3d4fc7a122362e7798fab86f96633d4974d666c7
Joshua Harris <joshuakharris12@gmail.com>
2023-10-11 19:54:26 +0100

    update static server

--- static/static.go
+++ static/static.go
@@ -7,11 +7,10 @@ import (
 )
 
 func Run(port uint64) {
-	fileServer := http.FileServer(http.Dir("./assets"))
-	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
-
+	http.Handle("/", http.FileServer(http.Dir("./assets")))
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Default().Printf("Listening on port: %d", port)
 }
